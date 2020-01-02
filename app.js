fd5d02ca795c0d1237d5457b66953197f6559f10
Ryan Strobolakos <ryan.strobolakos@gmail.com>
2020-01-02 11:09:02 -0500

    adding game and startGame functions to app.js file

---
+++ app.js
@@ -0,0 +1,18 @@
+const game = () => {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  const startGame = () => {
+    const playButton = document.querySelector(".intro button");
+    const introScreen = document.querySelector(".intro");
+    const match = document.querySelector(".match");
+
+    playButton.addEventListener("click", () => {
+      introScreen.classList.add("fadeOut");
+    });
+  };
+
+  startGame();
+};
+
+game();
