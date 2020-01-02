fd322906be4cc1a70ab90b33191a03ba0cd44cfd
Ryan Strobolakos <ryan.strobolakos@gmail.com>
2020-01-02 11:23:41 -0500

    creating a playMatch function to generate computer selection at time that player selects option

--- app.js
+++ app.js
@@ -13,7 +13,23 @@ const game = () => {
     });
   };
 
+  const playMatch = () => {
+    const options = document.querySelectorAll(".options button");
+    const playerHand = document.querySelector(".player-hand");
+    const computerHand = document.querySelector(".computer-hand");
+
+    const computerOptions = ["rock", "paper", "scissors"];
+
+    options.forEach(option => {
+      option.addEventListener("click", function() {
+        const computerNumber = Math.floor(Math.random() * 3);
+        const computerChoice = computerOptions[computerNumber];
+      });
+    });
+  };
+
   startGame();
+  playMatch();
 };
 
 game();
